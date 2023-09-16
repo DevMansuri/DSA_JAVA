833eb43eca7387b1305a6f81fda8e2100cd41a7d
DevMansuri <nadimmansu@gmail.com>
2023-09-16 17:52:50 +0530

    Add new File : Area Of Rectangle

---
+++ AreaOfRectangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+/*
+Author : DevMansuri.
+Purpose : To Calculate Area of Rectangle.
+ */
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Width :");
+        double width = scanner.nextDouble();
+
+        System.out.print("Enter the length :");
+        double length = scanner.nextDouble();
+
+        double area = length * width ;
+        System.out.println("Area of Triangle is : " + area);
+
+    }
+}
