a9313a0ffa098b8a7cdde13f3304f1d6ebdbd63d
DevMansuri <nadimmansu@gmail.com>
2023-09-16 17:59:45 +0530

    Add new File : Area Of Parallelogram

--- AreaOfParallelogram.java
+++ AreaOfParallelogram.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class AreaOfParallelogram {
+import java.util.Scanner;
+/*
+Author : DevMansuri.
+Purpose : To Calculate Area of Parallelogram.
+ */
+public class AreaOfParallelogram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the base of Triangle :");
+        double base = scanner.nextDouble();
+
+        System.out.print("Enter the height of Triangle :");
+        double height = scanner.nextDouble();
+
+        double area = base * height;
+        System.out.println("Area of Parallelogram is : " + area);
+        scanner.close();
+    }
 }
